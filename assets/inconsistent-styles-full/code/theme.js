9ac09da4e2d9590599dba61efcd7ecbf303b2120
Artem Sapegin <asapegin@wayfair.com>
2018-09-26 18:15:41 +0200

    Examples

--- assets/inconsistent-styles-full/code/theme.js
+++ assets/inconsistent-styles-full/code/theme.js
@@ -4,7 +4,11 @@ export default {
 	},
 	fontSizes: {
 		base: '1.7vmax',
-		s: '1.4vmax'
+		s: '1.4vmax',
+		m: '1.7vmax',
+		l: '2.0vmax',
+		xl: '2.5vmax',
+		xxl: '3.0vmax'
 	},
 	lineHeights: {
 		base: 1.5
